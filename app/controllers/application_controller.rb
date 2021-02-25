20d9c02f469b201441669e10b75b064476216a9c
figo5530 <figooooo0221@gmail.com>
2021-02-25 17:49:54 -0500

    commit tree a97a3f9214b82c088d2c7669ff3700b2462442f1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,43 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  get '/recipes' do
+    @r = Recipe.all
+    erb :index
+  end
 
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  get '/recipes/:id' do
+    # binding.pry
+    @r = Recipe.find(params[:id])
+    erb :show
+  end
+  
+  get '/recipes/:id/edit' do
+    @r = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    @r = Recipe.find(params[:id])
+    @r.name = params[:name]
+    @r.ingredients = params[:ingredients]
+    @r.cook_time = params[:cook_time]
+    @r.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  delete '/recipes/:id' do
+    @r = Recipe.find(params[:id])
+    @r.delete
+    redirect to '/recipes'
+  end
 end
